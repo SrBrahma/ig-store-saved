460faa053f21028516b1291e5b064bf78bd86998
Henrique Bruno <henrique.bruno.fa@gmail.com>
2021-05-12 20:27:56 -0300

    1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,142 +1,244 @@
 import { IgApiClient, SavedFeedResponseMedia } from 'instagram-private-api';
-import fs from 'fs-extra';
+// import fs from 'fs-extra';
+import mkdirp from 'mkdirp';
+import { DownloaderHelper } from 'node-downloader-helper';
+import globby from 'globby';
+import prompts from 'prompts';
+import { getDirDateName, getCarouselDirName, getCarouselFilename, getFilename, getMediaUserDateDirRelPath } from './pathsAndNames';
 import Path from 'path';
 
-
-
-// TODO ignore owned media. Maybe have a file with each downloaded media id?
-
-
-
-// import Downloader from 'nodejs-file-downloader';
-import { DownloaderHelper } from 'node-downloader-helper';
-
+// TODO add https://github.com/Teamwork/node-auto-launch ?
 
 const ig = new IgApiClient();
 
-const username = 'henrique.bruno.fa';
-const password = 'Mariokart#I!N@S#$%';
+
+let newMediaCount = 0;
+let totalMediaCount = 1;
+
+const prettyMediaCounterZeroPads = 4;
+/** COf0dN0M8pU = 11 chars */
+function getPrettyMediaCounter(code: string) {
+  return `[${String(totalMediaCount).padStart(prettyMediaCounterZeroPads, '0')}, ${code}]`;
+}
+// const prettyMediaCounterChars = getPrettyMediaCounter('COf0dN0M8pU').length;
 
 
-// maybe use __dirname
-const outPath = Path.join(process.cwd(), 'data');
 
+const previousUserFiles: Record<string, string[] | undefined> = {};
 
-async function saveFile({ code, date, url }: {
+async function alreadyExists({ code, date, username, isFromCarousel = false, carouselCounter = 0, author }: {
+  author: string;
+  code: string;
+  date: Date;
+  isFromCarousel?: boolean;
+  carouselCounter?: number;
+  username: string;
+}): Promise<boolean> {
+  const dirName = getDirDateName({ date });
+  if (!previousUserFiles[dirName])
+    previousUserFiles[dirName] = await globby(getMediaUserDateDirRelPath({ username, date }));
+
+  const regex = isFromCarousel
+    ? new RegExp(`${getCarouselDirName({ author, code, date })}/${getCarouselFilename({ carouselCounter })}`)
+    : new RegExp(getFilename({ author, code, date }));
+
+  const exists = !!(previousUserFiles[dirName]!.find(filename => filename.search(regex) !== -1));
+
+  return exists;
+}
+
+/** Will store on the local machine the media. */
+async function saveMediaFile({ code, date, url, username, isFromCarousel = false, carouselCounter = 0, author }: {
+  author: string;
   code: string;
   date: Date;
   url: string;
+  isFromCarousel?: boolean;
+  carouselCounter?: number;
+  username: string;
 }) {
+  // If we need, we can fetch the file extension from the url with the regex "/\.\w+\?/". This will output for ex ".mp4"
 
-  // https://stackoverflow.com/a/29774197/10247962
-  const offset = date.getTimezoneOffset();
-  const offsettedDate = new Date(date.getTime() - (offset*60*1000));
-  const YYYYmmDD = offsettedDate.toISOString().split('T')[0]!;
+  const outDirPath = isFromCarousel
+    ? Path.join(getMediaUserDateDirRelPath({ username, date }), getCarouselDirName({ author, date, code }))
+    : Path.join(getMediaUserDateDirRelPath({ username, date }));
 
-  const HHmmSS = offsettedDate.toISOString().split('T')[1]!.split('.')[0]!;
+  await mkdirp(outDirPath);
 
-  const YYYYmm = YYYYmmDD.substr(0, 7); // 2021-05
+  const filename = isFromCarousel
+    ? getCarouselFilename({ carouselCounter })
+    : getFilename({ author, code, date });
 
-
-  const filename = `${YYYYmmDD} ${HHmmSS} ${code}`;
-
-  const outDirPath = Path.join(outPath, YYYYmm);
-  // await fs.ensureDir(outDirPath);
-
-  // const outFilePath = Path.join(outDirPath, 'noiceFilename.jpg');
-
-  await fs.ensureDir(outDirPath);
   const download = new DownloaderHelper(url, outDirPath, {
     // override: ?
     fileName: {
       name: filename,
-      // ext,
+      // ext, // dont define to keep it
     } as any,
     override: true,
   });
 
   await download.start();
+
 }
 
 
-let mediaCounter = 1;
+// Using if/elseif instead of switches because they were looking ugly.
+async function parseData({ data, username }: {
+  data: SavedFeedResponseMedia,
+  username: string;
+}): Promise<void> {
 
-
-async function parseData(dataArray: SavedFeedResponseMedia[]): Promise<void> {
-
-  for (const data of dataArray) {
-
-    // https://stackoverflow.com/a/55022113/10247962
-    const date = new Date(data.taken_at * 1000);
-    const code = data.code; // shorter than id
-
-    // const id = data.
-    switch (data.media_type) {
-    case 1: { // Photo/Image
-      // Get first / largest / original image
-      const url = data.image_versions2?.candidates[0]?.url;
-      if (!url)
-        throw `Image without url! Media id = ${data.id}`;
-
-      console.log(`[${String(mediaCounter).padStart(4, '0')}] Salvando imagem... (code = ${code})`);
-      await saveFile({ code, url, date });
-      mediaCounter++;
-    } break;
-
-    case 2: { // Video
-      const url = data.video_versions?.[0]?.url;
-      if (!url)
-        throw `Video without url! Media id = ${data.id}`;
-
-      console.log(`[${String(mediaCounter).padStart(4, '0')}] Salvando vídeo...  (code = ${code})`);
-      await saveFile({ code, url, date });
-      mediaCounter++;
-    } break;
-
-    case 8:
-      throw 'Media type is "caroussel", invalid! Contact the developer of this program!';
-
-    default:
-      throw `Unknown media type! Contact the developer of this program! (its value is ${data.media_type})`;
-
+  /** Stores the file if not already stored. */
+  async function saveIfDontExist({ url, storingMessage, isFromCarousel, carouselCounter }: {
+    url: string;
+    /** If file doesn't exist, will print this before storing the file. The media counter
+     * will be appended to the beggining of the string. */
+    storingMessage: string;
+    isFromCarousel?: boolean;
+    carouselCounter?: number;
+  }): Promise<void> {
+    if (!(await alreadyExists({ code, date, username, author }))) {
+      console.log(`${getPrettyMediaCounter(code)} ${storingMessage}`);
+      await saveMediaFile({ code, url, date, username, author, isFromCarousel, carouselCounter });
+      newMediaCount++; // New file!
     }
-
+    totalMediaCount++;
   }
+
+  // https://stackoverflow.com/a/55022113/10247962
+  const date = new Date(data.taken_at * 1000);
+  const code = data.code; // shorter than data.id
+  const author = data.user.username;
+
+  if (data.media_type === 1) { // Photo/Image
+    // Get first / largest / original image
+    const url = data.image_versions2?.candidates[0]?.url;
+    if (!url)
+      throw `Image without url! Media code = ${code}`;
+    await saveIfDontExist({ url, storingMessage: 'Salvando imagem...' });
+  }
+
+  else if (data.media_type === 2) { // Video
+    const url = data.video_versions?.[0]?.url;
+    if (!url)
+      throw `Video without url! Media code = ${code}`;
+    await saveIfDontExist({ url, storingMessage: 'Salvando vídeo...' });
+  }
+
+  else if (data.media_type === 8) { // Caroussel
+    const carousel_media = data.carousel_media ?? [];
+    let carouselCounter = 1;
+
+    for (const media of carousel_media) {
+
+      if (media.media_type === 1) { // Photo/Image
+        const url = media.image_versions2.candidates[0]?.url;
+        if (!url)
+          throw `Image without URL! Carousel code = ${code}, counter = ${carouselCounter}`;
+        await saveIfDontExist({ url, carouselCounter, isFromCarousel: true,
+          storingMessage: `Salvando imagem do carrossel (${carouselCounter++})...` });
+      }
+      else if (media.media_type === 2) { // Video
+        const url = media.video_versions?.[0]?.url;
+        if (!url)
+          throw `Video without url! Carousel code = ${code}, counter = ${carouselCounter}`;
+
+        await saveIfDontExist({ url, carouselCounter, isFromCarousel: true,
+          storingMessage: `Salvando vídeo do carrossel (${carouselCounter++})...` });
+      }
+      else {
+        throw `Unknown media_type = ${media.media_type} inside the carousel of code =${code}`;
+      }
+
+    } // End of carousel for
+
+  } // End of carousel if
+
+  else {
+    throw `Unknown media_type = ${data.media_type}), media code = ${code}`;
+  }
+
 }
+
+
+
+
 
 
 async function main() {
   console.log('-=- Instagram Saved Media Saver -=-');
 
-  ig.state.generateDevice(username);
 
-  console.log('Fazendo login...');
-  await ig.simulate.preLoginFlow();
-  /** const loggedInUser = */ await ig.account.login(username, password);
+  let logged = false;
+  let username: string = '';
+  let password: string = '';
+
+  do {
+    try {
+
+      // https://flaviocopes.com/javascript-destructure-object-to-existing-variable/
+      ({ username, password } = await prompts([
+        {
+          message: 'Digite o nome do usuário',
+          type: 'text',
+          name: 'username',
+        }, {
+          message: 'Digite a senha',
+          type: 'password',
+          name: 'password',
+        },
+      ]));
+
+      ig.state.generateDevice(username);
+
+      console.log('Fazendo login...');
+      await ig.simulate.preLoginFlow();
+      /** const loggedInUser = */ await ig.account.login(username, password);
+      logged = true;
+
+    } catch (err) {
+      console.error(err.message ?? err);
+    }
+  } while (!logged);
+
+
   // Optionally wrap it to process.nextTick so we dont need to wait ending of this bunch of requests
   process.nextTick(async () => await ig.simulate.postLoginFlow());
+
 
   console.log('Obtendo lista de mídas salvas...');
   // https://stackoverflow.com/a/58759660/10247962
   const savedFeed = ig.feed.saved();
 
 
+
   // console.log(savedFeed.)
   do {
     const savedPosts = await savedFeed.items();
     //  savedPosts.
-    await parseData(savedPosts);
+    for (const data of savedPosts)
+      await parseData({ data, username });
   }
   while (savedFeed.isMoreAvailable());
 
+
+  if (newMediaCount === 0)
+    console.log('Concluído! Não há mídias novas a serem adicionadas.');
+  else {
+    const newText = newMediaCount === 1
+      ? `Foi adicionada 1 mídia nova`
+      : `Foram adicionadas ${newMediaCount} mídias novas`;
+    const totalText = totalMediaCount === 1 // no need to check 0, 0 would also fall in `newMediaCount === 0`
+      ? `1 mídia`
+      : `${totalMediaCount} mídias`;
+    console.log(`Concluído! ${newText} de um total de ${totalText}`);
+  }
 }
 
 
 main()
-  .then(() => {
-    console.log('Concluído!');
-  })
   .catch(err => {
-    console.error(err);
+    console.error(err.message ?? err);
     process.exit(1);
   });
---
+++ src/pathsAndNames.ts
@@ -0,0 +1,75 @@
+import Path from 'path';
+import { timestampOffsetMinutes } from './consts';
+
+/* Dir structure
+  media v1
+   |- $username1
+   |-|- $YYYY-MM
+   |-|-|- $DD $author $mediaCode .ext // .jpg | .mp4
+   |-|-|- $DD $author $mediaCode // carousel
+   |-|-|-|- $carouselCounter .ext // starts from 1. .jpg | .mp4
+    - $username2
+      - ...
+*/
+
+
+/* Careful with globby: https://github.com/sindresorhus/globby/issues/155#issuecomment-727533529
+ *
+ * We are using relative paths and Path.posix due to the issue above. */
+
+/** Naming it media vX so we can change the version for breaking changes without
+ * messing existing local data. */
+export const mediaDirName = 'media v1';
+
+export function getMediaUserDirRelPath({ username, forcePosix = false }: {
+  username: string;
+  /** Set forcePosix: true for globby. Else leave false for automatic path joining */
+  forcePosix?: boolean;
+}): string {
+  return forcePosix ? Path.posix.join('media v1', username) : Path.join('media v1', username);
+}
+
+export function getMediaUserDateDirRelPath({ username, date, forcePosix = false }: {
+  username: string;
+  date: Date;
+  /** Set forcePosix: true for globby. Else leave false for automatic path joining */
+  forcePosix?: boolean;
+}): string {
+  return forcePosix
+    ? Path.posix.join(getMediaUserDirRelPath({ username, forcePosix }), getDirDateName({ date }))
+    : Path.join(getMediaUserDirRelPath({ username, forcePosix }), getDirDateName({ date }));
+}
+
+/** Returns the YYYY-MM parent directory of the media. */
+export function getDirDateName({ date }: {date: Date}): string {
+  const offsettedDate = new Date(date.getTime() - (timestampOffsetMinutes * 60 * 1000));
+  const YYYY_MM_DD = offsettedDate.toISOString().split('T')[0]!;
+  const YYYY_MM = YYYY_MM_DD.substr(0, 7); // 2021-05
+  return YYYY_MM;
+}
+
+/** Without extension. */
+export function getFilename({ date, code, author }: {
+  date: Date, code: string, author: string
+}): string {
+  // https://stackoverflow.com/a/29774197/10247962
+  const offsettedDate = new Date(date.getTime() - (timestampOffsetMinutes * 60 * 1000));
+  // const YYYYmmDD = offsettedDate.toISOString().split('T')[0]!;
+  // const HHmmSS = offsettedDate.toISOString().split('T')[1]!.split('.')[0]!;
+  const dd = String(offsettedDate.getDate()).padStart(2, '0');
+  const filename = `${dd} ${author} ${code}`;
+
+  return filename;
+}
+
+/** Will call getFilename(). */
+export function getCarouselDirName({ date, code, author }: {
+  date: Date, code: string; author: string;
+}): string {
+  return getFilename({ date, code: code, author });
+}
+
+/** Will just return the carouselCounter as string. Won't include the file extension. */
+export function getCarouselFilename({ carouselCounter }: {carouselCounter: number}): string {
+  return String(carouselCounter);
+}
